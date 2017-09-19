045d1d8c12f916378919ee1af8459d57c5025e6a
Yauheni Sheima <Yauheni_Sheima@epam.com>
2017-09-19 13:30:44 +0300

    Works. c done

--- IntroductionConsole/Program.cs
+++ IntroductionConsole/Program.cs
@@ -8,45 +8,46 @@ namespace IntroductionConsole
 {
     class Program
     {
-        int _firstNumber = 0;
-        int _secondNumber = 0;
+        private int _firstNumber = 0;
+        private int _secondNumber = 0;
+
+//        public int FirstNumber { get; set; }
+//        public int SecondNumber { get; set; }
+
         static void Main(string[] args)
         {
             Program program = new Program();
             Console.WriteLine("Enter first number");
-            int firstNumber = program.CheckFirstNumber();
+            program.CheckFirstNumber();
             Console.WriteLine("Enter second number");
-            int secondNumber = program.CheckSecondNumber();
-            
-            Console.WriteLine("Sum is: " + (firstNumber + secondNumber));
+            program.CheckSecondNumber();
+
+            Console.WriteLine("Sum is: " + (program._firstNumber + program._secondNumber));
 
         }
 
-        private int CheckFirstNumber()
+        private void CheckFirstNumber()
         {
             string firstNumberStr = Console.ReadLine();
-            int firstNumber = 0;
-            if (firstNumberStr == null || int.TryParse(firstNumberStr, out firstNumber) == false)
-            {   
+            //            int firstNumber = 0;
+            
+            if (firstNumberStr == null || int.TryParse(firstNumberStr, out _firstNumber) == false)
+            {
                 Console.WriteLine("Enter first number again!");
                 CheckFirstNumber();
             }
-
-            return firstNumber;
         }
 
-        private int CheckSecondNumber()
+        private void CheckSecondNumber()
         {
             string secondNumberStr = Console.ReadLine();
-            int secondNumber= 0;
+//            int secondNumber= 0;
 
-            if (secondNumberStr == null || int.TryParse(secondNumberStr, out secondNumber) == false)
+            if (secondNumberStr == null || int.TryParse(secondNumberStr, out _secondNumber) == false)
             {
                 Console.WriteLine("Enter second number again!");
                 CheckSecondNumber();
             }
-
-            return secondNumber;
         }
     }
 }
